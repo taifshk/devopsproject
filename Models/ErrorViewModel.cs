0aa115f02d958c4c86f755b5414585aca12e87ef
Taifur Shaikh <taifur.shk@gmail.com>
2026-04-19 16:36:04 +0530

    Trigger Pipeline

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DevOpsProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
